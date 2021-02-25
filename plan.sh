08b9604fa5fa247f9ae9a5d252ab35c8b9333895
Chris Alfano <chris@jarv.us>
2021-02-25 23:50:21 +0000

    feat: add initial implementation

---
+++ plan.sh
@@ -0,0 +1,36 @@
+pkg_name=lens-composer-install
+pkg_origin=holo
+pkg_version="1.10"
+pkg_maintainer="Chris Alfano <chris@jarv.us>"
+pkg_license=("MIT")
+
+pkg_deps=(
+  core/bash
+  core/composer/1.10.5
+  jarvus/hologit
+)
+
+pkg_bin_dirs=(bin)
+
+
+do_build() {
+  return 0
+}
+
+do_build() {
+
+  pushd "${CACHE_PATH}" > /dev/null
+    build_line "Preparing bin scripts"
+    mkdir -v "bin"
+    cp -v "${PLAN_CONTEXT}/bin"/* "./bin/"
+    fix_interpreter "bin/*" core/bash bin/bash
+  popd > /dev/null
+}
+
+do_install() {
+  cp -r "${CACHE_PATH}"/* "${pkg_prefix}/"
+}
+
+do_strip() {
+  return 0
+}
